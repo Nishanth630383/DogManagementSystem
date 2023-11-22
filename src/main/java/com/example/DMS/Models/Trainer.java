e39fd4f923c3b5deb81bee377c034bc3cbe66559
Nishanth Raju <s559217@nwmissouri.edu>
2023-11-22 17:32:03 -0600

    updated pom.xml

--- src/main/java/com/example/DMS/Models/Trainer.java
+++ src/main/java/com/example/DMS/Models/Trainer.java
@@ -1,7 +1,9 @@
 package com.example.DMS.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Trainer {
